e861dc6211528270e84d157419527f2ede632722
Sergey Pelyovin <spelyovin12@mail.ru>
2021-07-09 01:40:49 +0300

    До зашрузки данных

---
+++ pages/posts.js
@@ -0,0 +1,28 @@
+import {useState, useEffect} from 'react'
+import {MainLayout} from "../components/MainLayout";
+
+export default function Posts(){
+
+    const [posts, setPosts] = useState([])
+
+    useEffect( () => {
+        async function load(){
+            const response = await fetch ('http://localhost:4200/posts')
+            const json = await response.json()
+            setPosts(json)
+        }
+
+        load()
+    }, [])
+
+    return (
+        <MainLayout title={'Post Page'}>
+            <h1>Posts Page</h1>
+
+            <pre>
+                {JSON.stringify(posts, null, 2)}
+            </pre>
+
+        </MainLayout>
+    )
+}
